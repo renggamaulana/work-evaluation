ff1196ec3c2d34c335744658ca956b3d7c5f70d3
rengga.maulana <rengga.maulana@tristansolutech.co.id>
2026-09-10 14:44:58 +0700

    update

--- data/content.ts
+++ data/content.ts
@@ -16,7 +16,7 @@ export const coverContent = {
   // highlightedWord: '3-Month',
   terminalLine: 'root@rengga:~/performance-review-2026 --init',
   candidate: 'Rengga Maulana',
-  evaluationDate: 'Friday, 28 August 2026',
+  evaluationDate: 'Wednesday, 10 September 2026',
   hint: 'Press → or Space to begin',
 };
 
